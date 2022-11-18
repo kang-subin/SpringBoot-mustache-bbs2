89ad0d7806d53dad8cddf08fdcb8edaa13c4ba22
강수빈 <tnbin98@gmail.com>
2022-11-18 14:20:27 +0900

    form input keyword, method query 구현

--- src/main/java/com/example/bbs4/repository/Hospitalrepository.java
+++ src/main/java/com/example/bbs4/repository/Hospitalrepository.java
@@ -1,6 +1,8 @@
 package com.example.bbs4.repository;
 
 import com.example.bbs4.domain.entity.Hospital;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,10 +10,11 @@ import java.util.List;
 public interface Hospitalrepository extends JpaRepository <Hospital, Integer> {
     List<Hospital> findByBusinessTypeNameIn(List<String> businessTypes);
 
-  List<Hospital> findBypatientRoomCountBetween(int patientRoomCount, int patientRoomCount2);
+    List<Hospital> findBypatientRoomCountBetween(int patientRoomCount, int patientRoomCount2);
 
     List<Hospital> findByPatientRoomCountBetweenOrderByPatientRoomCountDesc(int a, int b); // Between으로 병상수 10 ~20 미만을 조회하고, orderBy로 내림차순 정렬
 
+    Page<Hospital> findByRoadNameAddressContaining(String keyword, Pageable pageable); // 도로명 주소로 찾을 수 있도록 하는 메소드
 
 }
 
